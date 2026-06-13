2754ebb58913a315be348c8aa71dbffdee7e6b0f
MusicAppsUk <tim@carringtons.org>
2026-06-13 20:05:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'keymaster-pro-rc3-v32';
+const CACHE_NAME = 'keymaster-pro-rc3-v33';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
